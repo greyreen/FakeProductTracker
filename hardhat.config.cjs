c528cbe8f2c3b76bd07f42657fd318139dbed4c4
Greyreen <greyreen@Greyreen-MacBook-Air.local>
2025-10-25 18:26:11 +0300

    docs: Finalize README content and project cleanup

--- hardhat.config.cjs
+++ hardhat.config.cjs
@@ -1,4 +1,8 @@
+// Ethers eklentisini buraya require etmeliyiz
+require("@nomicfoundation/hardhat-ethers");
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.20",
+  // Gerekirse başka bir ayar ekleyebiliriz
 };
